a256773b00e65601fac614e5dd001d4e2c591917
tomoki737 <tomoki.k2159@gmail.com>
2021-11-28 14:02:48 +0900

    commimntのmigrationの作成

--- database/migrations/2021_11_28_042137_create_likes_table.php
+++ database/migrations/2021_11_28_042137_create_likes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSympathiesTable extends Migration
+class CreateLikesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -30,6 +30,6 @@ class CreateSympathiesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('sympathies');
+        Schema::dropIfExists('likes');
     }
 }
