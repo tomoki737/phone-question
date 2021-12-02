b78e2a5f3df62d01caa99277c1846e2ac89942fd
tomoki737 <tomoki.k2159@gmail.com>
2021-12-02 22:01:25 +0900

    テストの記述 #30

--- app/User.php
+++ app/User.php
@@ -43,12 +43,12 @@ class User extends Authenticatable
 
     public function followers(): BelongsToMany
     {
-        return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id');
+        return $this->belongsToMany('App\User', 'follows', 'followee_id', 'follower_id')->withTimestamps();
     }
 
     public function followings(): BelongsToMany
     {
-        return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id');
+        return $this->belongsToMany('App\User', 'follows', 'follower_id', 'followee_id')->withTimestamps();
     }
 
 
---
+++ tests/Feature/UserTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+use App\User;
+
+class UserTest extends TestCase
+{
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->user = factory(User::class)->create();
+        $this->anotherUser = factory(User::class)->create();
+    }
+
+    public function testIsFollowedByTheUser() {
+        $this->user->followings()->attach($this->anotherUser);
+        $result = $this->anotherUser->isFollowedBy($this->user);
+        $this->assertTrue($result);
+    }
+
+    public function testIsFollowedByNull() {
+        $result = $this->user->followings()->attach(null);
+        $this->assertFalse($result);
+    }
+}
