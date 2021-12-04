5f5cca2ec1e1bb72eb8d2a93825f9b89f63df753
tomoki737 <tomoki.k2159@gmail.com>
2021-12-04 13:28:43 +0900

    解決未解決で表示

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -8,7 +8,13 @@ class HomeController extends Controller
 {
     public function index()
     {
-        $questions = Question::all()->sortByDesc('created_at');
+        $questions = Question::whereNotNull('best_answer')->get();
         return view('home', ['questions' => $questions]);
+    }
+
+    public function un_solve()
+    {
+        $questions = Question::whereNull('best_answer')->get();
+        return view('un_solve', ['questions' => $questions]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 Route::get('/', 'HomeController@index')->name('home');
+Route::get('/un_solve', 'HomeController@un_solve')->name('un_solve');
 Route::get('/users', 'UserController@index')->name('users.index');
 Route::resource('/questions', 'QuestionController', ['except' => ['index', 'show']])->middleware('auth');
 Route::resource('/questions', 'QuestionController')->only(['show']);
